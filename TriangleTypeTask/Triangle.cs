5f115fd54470819c6b429f034b7203cdc2a7e1a0
Nikita Biletsky <nikitabiletsky@bk.ru>
2023-11-30 22:38:58 +0300

    init

---
+++ TriangleTypeTask/Triangle.cs
@@ -0,0 +1,53 @@
+﻿namespace TriangleTypeTask
+{
+    public class Triangle
+    {
+        private double SideA { get; }
+        private double SideB { get; }
+        private double SideC { get; }
+        public TriangleType TriangleType { get => GetTriangleType(); }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        private TriangleType GetTriangleType()
+        {
+            if (!CheckIsValid())
+            {
+                return TriangleType.None;
+            }
+
+            var maxSide = Math.Max(Math.Max(SideA, SideB), SideC);
+            var minSide = Math.Min(Math.Min(SideA, SideB), SideC);
+            var midSide = SideA + SideB + SideC - maxSide - minSide;
+
+            var sqrdMaxSide = Math.Pow(maxSide, 2);
+            var sqrdMinSide = Math.Pow(minSide, 2);
+            var sqrdMidSide = Math.Pow(midSide, 2);
+
+            if (sqrdMaxSide > sqrdMinSide + sqrdMidSide)
+            {
+                return TriangleType.ObtuseAngled;
+            }
+            else if (sqrdMaxSide == sqrdMinSide + sqrdMidSide)
+            {
+                return TriangleType.Rectangular;
+            }
+            else
+            {
+                return TriangleType.Acute;
+            }
+        }
+
+        private bool CheckIsValid()
+        {
+            return SideA + SideB > SideC
+                && SideA + SideC > SideB
+                && SideB + SideC > SideA;
+        }
+    }
+}
---
+++ TriangleTypeTask/TriangleType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleTypeTask
+{
+    public enum TriangleType
+    {
+        None,
+        ObtuseAngled,
+        Rectangular,
+        Acute
+    }
+}
